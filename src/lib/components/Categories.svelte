cb8a4f66f352d296a81c8161a2719cd24e151ab3
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2025-01-17 23:21:40 +0100

    add component Categories

---
+++ src/lib/components/Categories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	let currentPage: string = $page.url.pathname;
+
+	const categories: Record<string, string> = {
+		capitals: 'Capitales',
+		countries: 'Pays',
+		flags: 'Drapeaux',
+		general_knowledge: 'Culture générale'
+	};
+</script>
+
+<ul class="flex flex-col items-center gap-4">
+	{#each Object.keys(categories) as category}
+		<li>
+			<a href={`${currentPage}/${category}`}>{categories[category]}</a>
+		</li>
+	{/each}
+</ul>
