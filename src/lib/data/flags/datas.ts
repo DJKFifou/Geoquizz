bbfd8cad32dfeded1f63f4ee72aeb1dcfa1d10c2
Maxime <lust.maxime@outlook.fr>
2025-01-21 12:00:03 +0100

    update datas.ts for flags

--- src/lib/data/flags/datas.ts
+++ src/lib/data/flags/datas.ts
@@ -1,21 +1,25 @@
 const svgs = import.meta.glob("./svgs/*.svg");
-import easy from './easy.json';
 
-export async function getEasy() {
-    const svgPromises = Object.keys(svgs).reduce((acc, key) => {
-        acc[key] = svgs[key]();
-        return acc;
-    }, {} as Record<string, Promise<any>>);
+const svgPromises = Object.keys(svgs).reduce((acc, key) => {
+    acc[key] = svgs[key]();
+    return acc;
+}, {} as Record<string, Promise<any>>);
 
-    const svgModules = await Promise.all(Object.values(svgPromises));
+const svgModules = await Promise.all(Object.values(svgPromises));
 
-    const loadedSvgs = Object.keys(svgPromises).reduce((acc, key, index) => {
-        acc[key] = svgModules[index].default;
-        return acc;
-    }, {} as Record<string, string>);
+const loadedSvgs = Object.keys(svgPromises).reduce((acc, key, index) => {
+    acc[key] = svgModules[index].default;
+    return acc;
+}, {} as Record<string, string>);
 
-    return easy.map((question: any) => {
-        const svgPath = `./svgs/${question.answer.toLowerCase()}.svg`;
-        return { ...question, image: loadedSvgs[svgPath] };
+export async function getFlags(difficulty: any) {
+
+    return difficulty.map((item: any) => {
+        const svgPath = `./svgs/${item.answer.toLowerCase()}.svg`;
+        return { 
+            ...item, 
+            image: loadedSvgs[svgPath], 
+            question: "À quel pays appartient ce drapeau ?"
+        };
     });
 }
