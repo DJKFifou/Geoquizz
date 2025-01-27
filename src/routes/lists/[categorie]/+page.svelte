dfcc355426c3d37c433ff9550430649a60076f6e
DJKFifou <94984112+DJKFifou@users.noreply.github.com>
2025-01-27 21:08:58 +0100

    update configuration for flags & countries categorie

--- src/routes/lists/[categorie]/+page.svelte
+++ src/routes/lists/[categorie]/+page.svelte
@@ -1,17 +1,54 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	export let data;
+	type Country = {
+		country: string;
+		capital?: string[];
+		image?: string;
+	};
+	export let data: { data: Country[] };
 
 	$: categorieName = $page.params.categorie;
 
-	let datas = data?.data || [];
+	let datas: Country[] = data?.data || [];
 	let inputValue: string = '';
-	let revealedCapitals: string[] = [];
-	let missingCapitals: string[] = [];
+	let revealedAnswers: string[] = [];
+	let missingAnswers: string[] = [];
+	let lastCountrySelected: string = datas[0]?.country || '';
+	let currentCountryIndex: number = 0;
 	let timer: number = 15 * 60;
 	let timerInterval: NodeJS.Timeout | null = null;
-	let gameOver: boolean = false;
 	let timerDisabled: boolean = false;
+	let gameOver: boolean = false;
+
+	function handleCountryClick(country: string, index: number) {
+		inputValue = '';
+		lastCountrySelected = country;
+		currentCountryIndex = index;
+	}
+
+	function handleTabKey(e: KeyboardEvent) {
+		if (e.key === 'Tab') {
+			e.preventDefault();
+			if (datas.length > 0) {
+				currentCountryIndex = (currentCountryIndex + 1) % datas.length;
+				handleCountryClick(datas[currentCountryIndex].country, currentCountryIndex);
+			}
+		}
+	}
+
+	function moveToNextCountry() {
+		const nextUnrevealedCountryIndex = datas.findIndex(
+			(item, index) => !revealedAnswers.includes(item.country) && index > currentCountryIndex
+		);
+
+		if (nextUnrevealedCountryIndex !== -1) {
+			currentCountryIndex = nextUnrevealedCountryIndex;
+			handleCountryClick(datas[currentCountryIndex].country, currentCountryIndex);
+		} else {
+			currentCountryIndex = (currentCountryIndex + 1) % datas.length;
+			handleCountryClick(datas[currentCountryIndex].country, currentCountryIndex);
+		}
+	}
 
 	function cleanString(str: string): string {
 		return str
@@ -61,33 +98,25 @@
 					item.capital.some((cap: any) => cleanString(cap) === cleanString(inputValue))
 				);
 
-				if (matchingCountry && !revealedCapitals.includes(matchingCountry.country)) {
-					revealedCapitals = [...revealedCapitals, matchingCountry.country];
+				if (matchingCountry && !revealedAnswers.includes(matchingCountry.country)) {
+					revealedAnswers = [...revealedAnswers, matchingCountry.country];
 					inputValue = '';
 				}
-			} else {
-				const matchingCountry = datas.find(
-					(item: any) => cleanString(item.country) === cleanString(inputValue)
-				);
-
-				if (matchingCountry && !revealedCapitals.includes(matchingCountry.country)) {
-					revealedCapitals = [...revealedCapitals, matchingCountry.country];
-					inputValue = '';
-				}
+			} else if (
+				lastCountrySelected &&
+				cleanString(inputValue) === cleanString(lastCountrySelected)
+			) {
+				revealedAnswers = [...revealedAnswers, lastCountrySelected];
+				lastCountrySelected = '';
+				inputValue = '';
+				moveToNextCountry();
 			}
 		} else {
-			missingCapitals = [];
+			missingAnswers = [];
 			datas.forEach((item: any) => {
-				if (categorieName === 'capitals') {
-					const isFound = revealedCapitals.includes(item.country);
-					if (!isFound) {
-						missingCapitals.push(item.country);
-					}
-				} else {
-					const isFound = revealedCapitals.includes(item.country);
-					if (!isFound) {
-						missingCapitals.push(item.country);
-					}
+				const isFound = revealedAnswers.includes(item.country);
+				if (!isFound) {
+					missingAnswers.push(item.country);
 				}
 			});
 		}
@@ -98,15 +127,13 @@
 	class="container mx-auto flex min-h-screen w-full flex-col items-center justify-center bg-gray-800 text-white"
 >
 	<div class="flex w-full flex-col gap-8 p-4">
-		<div
-			class="sticky top-0 flex flex-col items-center gap-4 border-b-2 border-dashed border-white bg-gray-800 pb-2 pt-4"
-		>
+		<div class="sticky top-0 flex flex-col items-center gap-4 bg-gray-800 pb-2 pt-4">
 			<p class="text-center text-2xl font-bold">
 				{gameOver
 					? 'Temps écoulé ! Jeu terminé.'
 					: `Temps restant : ${timerDisabled ? 'Désactivé' : formatTime(timer)}`}
 			</p>
-			<p class="text-center text-xl font-medium">{revealedCapitals.length} / {datas.length}</p>
+			<p class="text-center text-xl font-medium">{revealedAnswers.length} / {datas.length}</p>
 			<div class="flex gap-4">
 				{#if !gameOver}
 					{#if !timerDisabled}
@@ -130,24 +157,25 @@
 					class="w-full rounded-lg px-6 py-4 text-black"
 					bind:value={inputValue}
 					autofocus
+					onkeydown={handleTabKey}
 				/>
 			{/if}
 		</div>
 		{#if categorieName === 'capitals'}
 			<div class="grid grid-cols-2 gap-4">
-				{#each datas as item (item.country)}
+				{#each datas as item, index (item.country)}
 					<div class="rounded bg-gray-700 p-2">
 						<p>{item.country}</p>
 					</div>
 					<div class="rounded bg-gray-700 p-2">
 						<p
-							class:!text-green-400={gameOver && revealedCapitals.includes(item.country)}
-							class:!text-red-400={gameOver && missingCapitals.includes(item.country)}
+							class:!text-green-400={gameOver && revealedAnswers.includes(item.country)}
+							class:!text-red-400={gameOver && missingAnswers.includes(item.country)}
 						>
-							{#if revealedCapitals.includes(item.country)}
-								{item.capital.join(' / ')}
-							{:else if missingCapitals.includes(item.country)}
-								{item.capital.join(' / ')}
+							{#if revealedAnswers.includes(item.country)}
+								{item.capital?.join(' / ') || 'No capital available'}
+							{:else if missingAnswers.includes(item.country)}
+								{item.capital?.join(' / ') || 'No capital available'}
 							{/if}
 						</p>
 					</div>
@@ -155,19 +183,30 @@
 			</div>
 		{:else}
 			<div class="flex flex-wrap justify-center gap-4">
-				{#each datas as item (item.country)}
+				{#each datas as item, index (item.country)}
 					<div class="flex flex-col gap-4">
-						<div class="rounded bg-gray-700 p-2">
+						<div class="flex justify-center rounded bg-gray-700 p-2">
 							<img src={item.image} alt="Réponse" class="h-28 w-40" />
 						</div>
-						<div class="h-10 rounded bg-gray-700 p-2">
+						<div
+							role="button"
+							class="h-10 cursor-pointer rounded bg-gray-700 p-2 {!gameOver &&
+							currentCountryIndex === index
+								? 'bg-yellow-700'
+								: ''} "
+							onfocus={() => handleCountryClick(item.country, index)}
+							onclick={() => handleCountryClick(item.country, index)}
+							onkeydown={(e) =>
+								e.key === 'Enter' || e.key === ' ' ? handleCountryClick(item.country, index) : null}
+							tabindex="0"
+						>
 							<p
-								class:!text-green-400={gameOver && revealedCapitals.includes(item.country)}
-								class:!text-red-400={gameOver && missingCapitals.includes(item.country)}
+								class:!text-green-400={gameOver && revealedAnswers.includes(item.country)}
+								class:!text-red-400={gameOver && missingAnswers.includes(item.country)}
 							>
-								{#if revealedCapitals.includes(item.country)}
+								{#if revealedAnswers.includes(item.country)}
 									{item.country}
-								{:else if missingCapitals.includes(item.country)}
+								{:else if missingAnswers.includes(item.country)}
 									{item.country}
 								{/if}
 							</p>
