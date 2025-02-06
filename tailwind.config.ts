a00bf0f0809c79cf8e2c048d8af78f96eae6fb7f
Maxime <lust.maxime@outlook.fr>
2025-02-06 11:23:49 +0100

    add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				"white" : "#F2F5F8",
+				"grey" : "#405869",
+				"turquoise" : "#13D8CC"
+			},
+			borderRadius: {
+				"4xl" : "1.875rem",
+				"5xl" : "2.25rem"
+			},
+			fontFamily: {
+                sans: ["Sequel", ...defaultTheme.fontFamily.sans],
+            }
+		}
 	},
 
 	plugins: []
