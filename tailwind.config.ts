bee53ab3a4b4be6c98a6811fab89bf4a211d20b8
Maxime <lust.maxime@outlook.fr>
2025-02-06 14:56:22 +0100

    add screens, fontSize & maxWidth customizations in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,24 @@ export default {
 			},
 			fontFamily: {
                 sans: ["Sequel", ...defaultTheme.fontFamily.sans],
-            }
+            },
+			screens: {
+			  '4xs': '320px',
+			  '3xs': '380px',
+			  '2xs': '430px',
+			  xs: '530px',
+			},
+			fontSize: {
+			  '2xs': ['0.625rem', '0.875rem'],
+			  '3.5xl': ['2rem', '2.5rem'],
+			  '4.5xl': ['2.5rem', '2.813rem'],
+			  '5.5xl': ['3.375rem', '3.375rem'],
+			  '6.5xl': ['4rem', '3.4rem'],
+			  '8.5xl': ['7rem', '7.313rem'],
+			},
+			maxWidth: {
+			  screen: '100vw',
+			},
 		}
 	},
 
