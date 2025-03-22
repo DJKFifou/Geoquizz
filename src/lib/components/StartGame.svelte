5efa842b6a4341f734c97ea2e4bd52b9f08a57f8
Maxime Lust <maxime@favoreatdesign.com>
2025-03-22 11:57:35 +0100

    add StartGame component

---
+++ src/lib/components/StartGame.svelte
@@ -0,0 +1,27 @@
+<script>
+	const { categorie, difficulty, timer, play } = $props();
+</script>
+
+<div class="h-full w-full">
+	<h1>Règle du jeu :</h1>
+	<h3>Vous avez choisi le niveau {difficulty} de la catégorie {categorie}</h3>
+	<div>
+		<h4>Les règles du jeu sont les suivantes :</h4>
+		<ul></ul>
+	</div>
+	<div>
+		<h5>Voulez-vous mettre un timer ?</h5>
+		<div>
+			<div>
+				<label for="withoutTimer">Non</label>
+				<input type="radio" id="withoutTimer" checked />
+			</div>
+			<div>
+				<label for="withTimer">Oui</label>
+				<input type="radio" id="withTimer" />
+			</div>
+		</div>
+	</div>
+	<button onclick={play}>Jouer</button>
+	<p></p>
+</div>
